3dfd5be2330f1865249cac56ce75fff6c19ba3b9
srf1995 <739093968@qq.com>
2019-05-02 17:31:02 +0800

    1.0版本功能及外型

---
+++ js/javascript.js
@@ -0,0 +1,199 @@
+var hidden = document.getElementById("hidden");
+var show = document.getElementById("show");
+var A = document.getElementById("a");
+var B = document.getElementById("b");
+var C = document.getElementById("c");
+var D = document.getElementById("d");
+var E = document.getElementById("e");
+var F = document.getElementById("f");
+var G = document.getElementById("g");
+var H = document.getElementById("h");
+var I = document.getElementById("i");
+var J = document.getElementById("j");
+var K = document.getElementById("k");
+var L = document.getElementById("l");
+//显示 AC正义 子栏函数
+function showOne(){
+	hidden.id="show"
+	A.className="show"
+	B.className="hidden"
+	C.className="hidden"
+	D.className="hidden"
+	E.className="hidden"
+	F.className="hidden"
+	G.className="hidden"
+	H.className="hidden"
+	I.className="hidden"
+	J.className="hidden"
+	K.className="hidden"
+	L.className="hidden"
+}
+//显示 AC正义 子栏函数
+function showTwo(){
+	hidden.id="show"
+	A.className="hidden"
+	B.className="show"
+	C.className="hidden"
+	D.className="hidden"
+	E.className="hidden"
+	F.className="hidden"
+	G.className="hidden"
+	H.className="hidden"
+	I.className="hidden"
+	J.className="hidden"
+	K.className="hidden"
+	L.className="hidden"
+}
+function showThree(){
+	hidden.id="show"
+	A.className="hidden"
+	B.className="hidden"
+	C.className="show"
+	D.className="hidden"
+	E.className="hidden"
+	F.className="hidden"
+	G.className="hidden"
+	H.className="hidden"
+	I.className="hidden"
+	J.className="hidden"
+	K.className="hidden"
+	L.className="hidden"
+}
+function showFour(){
+	hidden.id="show"
+	A.className="hidden"
+	B.className="hidden"
+	C.className="hidden"
+	D.className="show"
+	E.className="hidden"
+	F.className="hidden"
+	G.className="hidden"
+	H.className="hidden"
+	I.className="hidden"
+	J.className="hidden"
+	K.className="hidden"
+	L.className="hidden"
+}
+function showFive(){
+	hidden.id="show"
+	A.className="hidden"
+	B.className="hidden"
+	C.className="hidden"
+	D.className="hidden"
+	E.className="show"
+	F.className="hidden"
+	G.className="hidden"
+	H.className="hidden"
+	I.className="hidden"
+	J.className="hidden"
+	K.className="hidden"
+	L.className="hidden"
+}
+function showSix(){
+	hidden.id="show"
+	A.className="hidden"
+	B.className="hidden"
+	C.className="hidden"
+	D.className="hidden"
+	E.className="hidden"
+	F.className="show"
+	G.className="hidden"
+	H.className="hidden"
+	I.className="hidden"
+	J.className="hidden"
+	K.className="hidden"
+	L.className="hidden"
+}
+function showSeven(){
+	hidden.id="show"
+	A.className="hidden"
+	B.className="hidden"
+	C.className="hidden"
+	D.className="hidden"
+	E.className="hidden"
+	F.className="hidden"
+	G.className="show"
+	H.className="hidden"
+	I.className="hidden"
+	J.className="hidden"
+	K.className="hidden"
+	L.className="hidden"
+}
+function showEight(){
+	hidden.id="show"
+	A.className="hidden"
+	B.className="hidden"
+	C.className="hidden"
+	D.className="hidden"
+	E.className="hidden"
+	F.className="hidden"
+	G.className="hidden"
+	H.className="show"
+	I.className="hidden"
+	J.className="hidden"
+	K.className="hidden"
+	L.className="hidden"
+}
+function showNine(){
+	hidden.id="show"
+	A.className="hidden"
+	B.className="hidden"
+	C.className="hidden"
+	D.className="hidden"
+	E.className="hidden"
+	F.className="hidden"
+	G.className="hidden"
+	H.className="hidden"
+	I.className="show"
+	J.className="hidden"
+	K.className="hidden"
+	L.className="hidden"
+}
+function showTen(){
+	hidden.id="show"
+	A.className="hidden"
+	B.className="hidden"
+	C.className="hidden"
+	D.className="hidden"
+	E.className="hidden"
+	F.className="hidden"
+	G.className="hidden"
+	H.className="hidden"
+	I.className="hidden"
+	J.className="show"
+	K.className="hidden"
+	L.className="hidden"
+}
+function showEleven(){
+	hidden.id="show"
+	A.className="hidden"
+	B.className="hidden"
+	C.className="hidden"
+	D.className="hidden"
+	E.className="hidden"
+	F.className="hidden"
+	G.className="hidden"
+	H.className="hidden"
+	I.className="hidden"
+	J.className="hidden"
+	K.className="show"
+	L.className="hidden"
+}
+function showTwelve(){
+	hidden.id="show"
+	A.className="hidden"
+	B.className="hidden"
+	C.className="hidden"
+	D.className="hidden"
+	E.className="hidden"
+	F.className="hidden"
+	G.className="hidden"
+	H.className="hidden"
+	I.className="hidden"
+	J.className="hidden"
+	K.className="hidden"
+	L.className="show"
+}
+/*function show_Hidden(){
+  show.id = "hidden"
+}*/
